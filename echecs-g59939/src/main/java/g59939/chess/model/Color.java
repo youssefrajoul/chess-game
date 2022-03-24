ea8788c7009dcaa8fee18cee5f192fc5dfed6ed7
59939 <59939@etu.he2b.be>
2022-03-24 10:20:51 +0100

    a continuer

---
+++ echecs-g59939/src/main/java/g59939/chess/model/Color.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g59939.chess.model;
+
+/**
+ *
+ * @author g59939
+ */
+public enum Color {
+    WHITE, BLACK;
+    
+    /**
+     *
+     * @return
+     */
+    public Color opposite () {
+        if (this == Color.WHITE) 
+            return BLACK;
+
+      return WHITE;
+
+    }
+}
