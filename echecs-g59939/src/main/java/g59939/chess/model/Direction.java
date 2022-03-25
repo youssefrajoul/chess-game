8c143273a6ea267353345f71cd81029988c75246
59939 <59939@etu.he2b.be>
2022-03-25 12:05:24 +0100

    Créer l’énumération Direction

---
+++ echecs-g59939/src/main/java/g59939/chess/model/Direction.java
@@ -0,0 +1,44 @@
+package g59939.chess.model;
+
+/**
+ *
+ * @author g59939
+ */
+public enum Direction {
+    NW(1, -1), N(1, 0), NE(1, 1), W(0, -1), E(0, 1), SW(-1, -1), S(-1, 0), SE(-1, 1);
+
+    private int deltaRow;
+    private int deltaColumn;
+
+    /**
+     * This method is used as a constructor of the Direction Class to initialize
+     * attributes (deltaRow, deltaColumn)
+     *
+     * @param deltaR this is the first parameter which represents the row
+     * movements
+     * @param deltaC this is the second parameter which represents the column
+     * movements
+     */
+    Direction(int deltaR, int deltaC) {
+        this.deltaRow = deltaR;
+        this.deltaColumn = deltaC;
+    }
+
+    /**
+     * this method gets the row movement(direction) of the pawn
+     *
+     * @return deltaRow the movement in the row
+     */
+    public int getDeltaRow() {
+        return deltaRow;
+    }
+
+    /**
+     * this method gets the column movement(direction) of the pawn
+     *
+     * @return deltaRow the movement in the column
+     */
+    public int getDeltaColumn() {
+        return deltaColumn;
+    }
+}
