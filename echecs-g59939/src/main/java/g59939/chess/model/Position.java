4c6ef90d20e63f8dd68c69dc7f033d6f47517c40
59939 <59939@etu.he2b.be>
2022-03-25 12:29:38 +0100

    3.4 a continuer

--- echecs-g59939/src/main/java/g59939/chess/model/Position.java
+++ echecs-g59939/src/main/java/g59939/chess/model/Position.java
@@ -10,11 +10,13 @@ package g59939.chess.model;
  * @author g59939
  */
 public class Position {
+
     private int row;
     private int column;
-    
+
     /**
      * This method is used as a constructor of the position Class
+     *
      * @param row of the position
      * @param column of the position
      */
@@ -22,20 +24,26 @@ public class Position {
         this.row = row;
         this.column = column;
     }
-    
+
     /**
      * this method gets the row of the position
+     *
      * @return row fo the position
      */
     public int getRow() {
         return row;
     }
-    
+
     /**
      * this method gets the column of the position
+     *
      * @return column of the position
      */
     public int getColumn() {
         return column;
+    }
+
+    public Position next(Direction dir) {
+        Position test = new Position(this.row + dir.getDeltaRow(), this.column + dir.getDeltaColumn());
     }
 }
