8a29b0dc82d11f5acca9ec8ea0039b87108a8d15
59939 <59939@etu.he2b.be>
2022-03-30 11:51:31 +0200

    Créer la classe Player

--- echecs-g59939/src/main/java/g59939/chess/model/Player.java
+++ echecs-g59939/src/main/java/g59939/chess/model/Player.java
@@ -7,11 +7,19 @@ package g59939.chess.model;
 public class Player {
     
     private Color color;
-
+    
+    /**
+     * Constructor to initialize the attribute of this class
+     * @param color 
+     */
     public Player(Color color) {
         this.color = color;
     }
-
+    
+    /**
+     * This getter method returns the player's color
+     * @return 
+     */
     public Color getColor() {
         return color;
     }
