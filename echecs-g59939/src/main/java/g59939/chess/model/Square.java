be4fc96d3b94903aa28931797fc6dd32eeb950a0
59939 <59939@etu.he2b.be>
2022-03-30 12:36:00 +0200

    Créer la classe Square

--- echecs-g59939/src/main/java/g59939/chess/model/Square.java
+++ echecs-g59939/src/main/java/g59939/chess/model/Square.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package g59939.chess.model;
 
 /**
@@ -11,4 +6,37 @@ package g59939.chess.model;
  */
 public class Square {
     
+    private Piece piece;
+
+    /**
+     * Constructor to initialize the attribute of this class
+     * @param piece 
+     */
+    public Square(Piece piece) {
+        this.piece = piece;
+    }
+
+    /**
+     * this getter returns the piece inside that Square
+     * @return 
+     */
+    public Piece getPiece() {
+        return piece;
+    }
+
+    /**
+     * This setter method sets the attribute piece of the square Class
+     * @param piece 
+     */
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+    
+    /**
+     * this method checks if the square is free or not
+     * @return true if it's free(no piece). or false when it isn't
+     */
+    public boolean isFree() {
+        return this.piece == null;
+    }
 }
