531ea81d3ad4fe02e83dfcd91679dfff795263f6
59939 <59939@etu.he2b.be>
2022-03-30 11:40:30 +0200

    Créer la classe Piece

--- echecs-g59939/src/main/java/g59939/chess/model/Piece.java
+++ echecs-g59939/src/main/java/g59939/chess/model/Piece.java
@@ -6,11 +6,19 @@ package g59939.chess.model;
  */
 public class Piece {
     private Color color;
-
+    
+    /**
+     * Constructor to initialize the attribute of this class
+     * @param color 
+     */
     public Piece(Color color) {
         this.color = color;
     }
-
+    
+    /**
+     * This getter method returns the pawn's color
+     * @return 
+     */
     public Color getColor() {
         return color;
     }
