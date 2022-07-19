c58ab88e25e6e91b95295abe7e8d9f79219131ac
59939 <59939@etu.he2b.be>
2022-07-19 20:34:33 +0200

    Créer l’énumération Color

---
+++ echecs-g59939/src/main/java/g59939/chess/model/Color.java
@@ -0,0 +1,20 @@
+package g59939.chess.model;
+
+/**
+ * Represents the color of the player
+ * @author g59939
+ */
+public enum Color {
+    WHITE, BLACK;
+    
+    /**
+     * Gets the opposite of the current color
+     * @return 
+     */
+    public Color opposite(){
+        if (this == Color.WHITE) {
+            return BLACK;
+        }
+        return WHITE;
+    }
+}
