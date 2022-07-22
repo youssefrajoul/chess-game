2b2f9dff93e631ab2d71ecdbcc22a67ec68d2187
59939 <59939@etu.he2b.be>
2022-07-22 10:30:02 +0200

    Créer la classe Square

---
+++ echecs-g59939/src/main/java/g59939/chess/model/Square.java
@@ -0,0 +1,41 @@
+package g59939.chess.model;
+
+/**
+ * Represents one square of the chess board
+ * @author HONOR
+ */
+public class Square {
+    private Piece piece;
+
+    /**
+     * constructor of the class
+     * @param piece 
+     */
+    public Square(Piece piece) {
+        this.piece = piece;
+    }
+
+    /**
+     * Gets the piece in the square
+     * @return 
+     */
+    public Piece getPiece() {
+        return piece;
+    }
+
+    /**
+     * Sets the piece in the square
+     * @param piece Object of type piece
+     */
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+    
+    /**
+     * Gets the square has a piece or not
+     * @return 
+     */
+    public boolean isFree() {
+        return piece == null;
+    }
+}
