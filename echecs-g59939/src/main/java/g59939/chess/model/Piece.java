7a0a8628f0f4ffb65b2dfbd44749a8e13739cfa1
59939 <59939@etu.he2b.be>
2022-07-26 07:43:31 +0200

    Créer et tester la méthode getPossibleMoves

--- echecs-g59939/src/main/java/g59939/chess/model/Piece.java
+++ echecs-g59939/src/main/java/g59939/chess/model/Piece.java
@@ -29,48 +29,67 @@ public class Piece {
         return color;
     }
 
-    private void whitePiecePossMoves(List<Position> possMovesList, Position position, Board board) {
-        if (board.getPiece(position).getColor() == Color.WHITE && board.contains(position.next(Direction.N))) {
-            if (board.isFree(position.next(Direction.N))) {
-                possMovesList.add(position.next(Direction.N));
-                if (board.isFree(position.next(Direction.N).next(Direction.N)) && position.getRow() == board.getInitialPawnRow(board.getPiece(position).getColor())) {
-                    possMovesList.add(position.next(Direction.N).next(Direction.N));
-                }
-            }
-            if (!board.isFree(position.next(Direction.NE)) && board.containsOppositeColor(position.next(Direction.NE), Color.WHITE)) {
-                possMovesList.add(position.next(Direction.NE));
-            }
-            if (!board.isFree(position.next(Direction.NW)) && board.containsOppositeColor(position.next(Direction.NW), Color.WHITE)) {
-                possMovesList.add(position.next(Direction.NW));
-            }
-        }
-    }
-
-    private void blackPiecePossMoves(List<Position> possMovesList, Position position, Board board) {
-        if (board.getPiece(position).getColor() == Color.BLACK && board.contains(position.next(Direction.S))) {
-            if (board.isFree(position.next(Direction.S))) {
-                possMovesList.add(position.next(Direction.S));
-                if (board.isFree(position.next(Direction.S).next(Direction.S)) && position.getRow() == board.getInitialPawnRow(board.getPiece(position).getColor())) {
-                    possMovesList.add(position.next(Direction.S).next(Direction.S));
-                }
-            }
-            if (!board.isFree(position.next(Direction.SE)) && board.containsOppositeColor(position.next(Direction.SE), Color.BLACK)) {
-                possMovesList.add(position.next(Direction.SE));
-            }
-            if (!board.isFree(position.next(Direction.SW)) && board.containsOppositeColor(position.next(Direction.SW), Color.BLACK)) {
-                possMovesList.add(position.next(Direction.SW));
-            }
-        }
-    }
-
+    /**
+     * Gets the list of possible Moves
+     * @param position Object of type Position
+     * @param board Object of type Board
+     * @return List of type Position
+     */
     public List<Position> getPossibleMoves(Position position, Board board) {
-        List<Position> possMovesList;
-        possMovesList = new ArrayList<>();
+        List<Position> possibleMoves;
+        possibleMoves = new ArrayList<>();
 
-        whitePiecePossMoves(possMovesList, position, board);
-        ///////////////////////
-        blackPiecePossMoves(possMovesList, position, board);
-        
-        return possMovesList;
+        whitePiecePossMoves(possibleMoves, position, board);
+
+        blackPiecePossMoves(possibleMoves, position, board);
+
+        return possibleMoves;
+    }
+
+    /**
+     * Sub-Method to got only white possible moves
+     * @param possibleMoves List of type position
+     * @param position object Position
+     * @param board Object Board
+     */
+    private void whitePiecePossMoves(List<Position> possibleMoves, Position position, Board board) {
+        if (board.getPiece(position).getColor() == Color.WHITE && board.contains(position.next(Direction.N))) {
+            // Vertical Moves
+            if (board.isFree(position.next(Direction.N))) {
+                possibleMoves.add(position.next(Direction.N));
+                if (board.isFree(position.next(Direction.N).next(Direction.N)) && position.getRow() == board.getInitialPawnRow(board.getPiece(position).getColor())) {
+                    possibleMoves.add(position.next(Direction.N).next(Direction.N));
+                }
+            }
+            possAttackMoves(possibleMoves, position, board, Direction.NE, Color.WHITE);
+            possAttackMoves(possibleMoves, position, board, Direction.NW, Color.WHITE);
+        }
+    }
+
+    private void blackPiecePossMoves(List<Position> possibleMoves, Position position, Board board) {
+        if (board.getPiece(position).getColor() == Color.BLACK && board.contains(position.next(Direction.S))) {
+            // Vertical Moves
+            if (board.isFree(position.next(Direction.S))) {
+                possibleMoves.add(position.next(Direction.S));
+                if (board.isFree(position.next(Direction.S).next(Direction.S)) && position.getRow() == board.getInitialPawnRow(board.getPiece(position).getColor())) {
+                    possibleMoves.add(position.next(Direction.S).next(Direction.S));
+                }
+            }
+            possAttackMoves(possibleMoves, position, board, Direction.SE, Color.BLACK);
+            possAttackMoves(possibleMoves, position, board, Direction.SW, Color.BLACK);
+        }
+    }
+
+    /**
+     * Sub-Method to get only the possible attacking Moves
+     * @param possibleMoves List of type position
+     * @param position object Position
+     * @param board Object Board
+     * @param direction object Direction
+     */
+    private void possAttackMoves(List<Position> possibleMoves, Position position, Board board, Direction direction, Color color) {
+        if (!board.isFree(position.next(direction)) && board.containsOppositeColor(position.next(direction), color)) {
+            possibleMoves.add(position.next(direction));
+        }
     }
 }
