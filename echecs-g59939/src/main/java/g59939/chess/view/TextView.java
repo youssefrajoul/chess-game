6268cb13202e8973d532c3e6e0c27eba1919eba5
59939 <59939@etu.he2b.be>
2022-08-01 19:52:13 +0200

    Class textView Update

--- echecs-g59939/src/main/java/g59939/chess/view/TextView.java
+++ echecs-g59939/src/main/java/g59939/chess/view/TextView.java
@@ -83,15 +83,15 @@ public class TextView implements View {
      */
     @Override
     public Position askPosition() {
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('a', 0);
-        map.put('b', 1);
-        map.put('c', 2);
-        map.put('d', 3);
-        map.put('e', 4);
-        map.put('f', 5);
-        map.put('g', 6);
-        map.put('h', 7);
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("a", 0);
+        map.put("b", 1);
+        map.put("c", 2);
+        map.put("d", 3);
+        map.put("e", 4);
+        map.put("f", 5);
+        map.put("g", 6);
+        map.put("h", 7);
         System.out.println("Veuillez entrez une position valide.");
         Scanner scan = new Scanner(System.in);
         System.out.println("Entrez un chiffre (1-8):");
@@ -102,16 +102,15 @@ public class TextView implements View {
         }
         int input1 = scan.nextInt() - 1;
         System.out.println("Entrez une lettre (a-h):");
-        char input2 = scan.next().charAt(0);
-        while (!map.containsKey(input2)) {
+        String input2 = scan.next();
+        while (!map.containsKey(input2) || input2.length()>1) {
             displayError("Rien qu'une seul lettre entre a et h (inclus) svp!!!");
             System.out.println("Entrez une lettre (a-h):");
-            input2 = scan.next().charAt(0);
+            input2 = scan.next();
         }
 
         Position pos = new Position(input1, map.get(input2));
         return pos;
-        /// to do // Lecture Robust
     }
 
     /**
