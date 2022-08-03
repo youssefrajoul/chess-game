594537515f0e1fd4a7dd0132735c8da98aa54beb
59939 <59939@etu.he2b.be>
2022-08-03 12:07:57 +0200

    Update la classe Knight

--- echecs-g59939/src/main/java/g59939/chess/model/pieces/Knight.java
+++ echecs-g59939/src/main/java/g59939/chess/model/pieces/Knight.java
@@ -5,10 +5,11 @@ import g59939.chess.model.Color;
 import g59939.chess.model.Direction;
 import g59939.chess.model.Position;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
- *
+ * Represents the knight of the chess game
  * @author g59939
  */
 public class Knight extends Piece {
@@ -18,109 +19,33 @@ public class Knight extends Piece {
     }
 
     /**
+     * Gets the list of possible moves for knight
      *
-     * @param position
-     * @param board
-     * @return
+     * @param position the current position of the knight
+     * @param board the chess game board
+     * @return a list of all possible moves for knight
      */
     @Override
     public List<Position> getPossibleMoves(Position position, Board board) {
         List<Position> possMovesList = new ArrayList<>();
 
-        directionNordEstNord(position, board, possMovesList);
-        directionNordEstEst(position, board, possMovesList);
-        directionNordWestNord(position, board, possMovesList);
-        directionNordWestWest(position, board, possMovesList);
-        directionSudEstSud(position, board, possMovesList);
-        directionSudEstEst(position, board, possMovesList);
-        directionSudWestSud(position, board, possMovesList);
-        directionSudWestWest(position, board, possMovesList);
+        HashMap<Integer, Position> map = new HashMap<>();
+        map.put(0, position.next(Direction.NE).next(Direction.N));
+        map.put(1, position.next(Direction.NE).next(Direction.E));
+        map.put(2, position.next(Direction.NW).next(Direction.N));
+        map.put(3, position.next(Direction.NW).next(Direction.W));
+        map.put(4, position.next(Direction.SE).next(Direction.S));
+        map.put(5, position.next(Direction.SE).next(Direction.E));
+        map.put(6, position.next(Direction.SW).next(Direction.S));
+        map.put(7, position.next(Direction.SW).next(Direction.W));
+        for (int i = 0; i < map.size(); i++) {
+            if (board.contains(map.get(i)) && board.isFree(map.get(i))) {
+                possMovesList.add(map.get(i));
+            }
+            if (board.contains(map.get(i)) && !board.isFree(map.get(i)) && getColor() != board.getPiece(map.get(i)).getColor()) {
+                possMovesList.add(map.get(i));
+            }
+        }
         return possMovesList;
-    }
-
-    /**
-     *
-     * @param position
-     * @param board
-     * @param possMovesList
-     */
-    private void directionNordEstNord(Position position, Board board, List<Position> possMovesList) {
-        Position posNEN = position.next(Direction.NE).next(Direction.N);
-        if (board.contains(posNEN) && board.isFree(posNEN)) {
-            possMovesList.add(posNEN);
-        }
-        if (board.contains(posNEN) && !board.isFree(posNEN) && getColor() != board.getPiece(posNEN).getColor()) {
-            possMovesList.add(posNEN);
-        }
-    }
-
-    private void directionNordEstEst(Position position, Board board, List<Position> possMovesList) {
-        Position posNEE = position.next(Direction.NE).next(Direction.E);
-        if (board.contains(posNEE) && board.isFree(posNEE)) {
-            possMovesList.add(posNEE);
-        }
-        if (board.contains(posNEE) && !board.isFree(posNEE) && getColor() != board.getPiece(posNEE).getColor()) {
-            possMovesList.add(posNEE);
-        }
-    }
-
-    private void directionNordWestNord(Position position, Board board, List<Position> possMovesList) {
-        Position posNWN = position.next(Direction.NW).next(Direction.N);
-        if (board.contains(posNWN) && board.isFree(posNWN)) {
-            possMovesList.add(posNWN);
-        }
-        if (board.contains(posNWN) && !board.isFree(posNWN) && getColor() != board.getPiece(posNWN).getColor()) {
-            possMovesList.add(posNWN);
-        }
-    }
-
-    private void directionNordWestWest(Position position, Board board, List<Position> possMovesList) {
-        Position posNWW = position.next(Direction.NW).next(Direction.W);
-        if (board.contains(posNWW) && board.isFree(posNWW)) {
-            possMovesList.add(posNWW);
-        }
-        if (board.contains(posNWW) && !board.isFree(posNWW) && getColor() != board.getPiece(posNWW).getColor()) {
-            possMovesList.add(posNWW);
-        }
-    }
-
-    private void directionSudEstSud(Position position, Board board, List<Position> possMovesList) {
-        Position posSES = position.next(Direction.SE).next(Direction.S);
-        if (board.contains(posSES) && board.isFree(posSES)) {
-            possMovesList.add(posSES);
-        }
-        if (board.contains(posSES) && !board.isFree(posSES) && getColor() != board.getPiece(posSES).getColor()) {
-            possMovesList.add(posSES);
-        }
-    }
-
-    private void directionSudEstEst(Position position, Board board, List<Position> possMovesList) {
-        Position posSEE = position.next(Direction.SE).next(Direction.E);
-        if (board.contains(posSEE) && board.isFree(posSEE)) {
-            possMovesList.add(posSEE);
-        }
-        if (board.contains(posSEE) && !board.isFree(posSEE) && getColor() != board.getPiece(posSEE).getColor()) {
-            possMovesList.add(posSEE);
-        }
-    }
-
-    private void directionSudWestSud(Position position, Board board, List<Position> possMovesList) {
-        Position posSWS = position.next(Direction.SW).next(Direction.S);
-        if (board.contains(posSWS) && board.isFree(posSWS)) {
-            possMovesList.add(posSWS);
-        }
-        if (board.contains(posSWS) && !board.isFree(posSWS) && getColor() != board.getPiece(posSWS).getColor()) {
-            possMovesList.add(posSWS);
-        }
-    }
-
-    private void directionSudWestWest(Position position, Board board, List<Position> possMovesList) {
-        Position posSWW = position.next(Direction.SW).next(Direction.W);
-        if (board.contains(posSWW) && board.isFree(posSWW)) {
-            possMovesList.add(posSWW);
-        }
-        if (board.contains(posSWW) && !board.isFree(posSWW) && getColor() != board.getPiece(posSWW).getColor()) {
-            possMovesList.add(posSWW);
-        }
     }
 }
