08c5e2ce1a69b7acba8d47389ae1f34f52f7380e
59939 <59939@etu.he2b.be>
2022-08-03 19:06:09 +0200

    Ajouter la classe Rook

--- echecs-g59939/src/main/java/g59939/chess/model/pieces/Rook.java
+++ echecs-g59939/src/main/java/g59939/chess/model/pieces/Rook.java
@@ -2,7 +2,9 @@ package g59939.chess.model.pieces;
 
 import g59939.chess.model.Board;
 import g59939.chess.model.Color;
+import g59939.chess.model.Direction;
 import g59939.chess.model.Position;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,7 +25,32 @@ public class Rook extends Piece {
 
     @Override
     public List<Position> getPossibleMoves(Position position, Board board) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Position> possMovesList = new ArrayList<>();
+        List<Direction> listOfDirections = List.of(
+                (Direction.N),
+                (Direction.E),
+                (Direction.W),
+                (Direction.S)
+        );
+        for (int i = 0; i < listOfDirections.size(); i++) {
+            for (int j = 0; j < listOfDirections.size(); j++) {
+                if (board.contains(nextPosition(j, listOfDirections.get(i), position))
+                        && board.isFree(nextPosition(j, listOfDirections.get(i), position))) {
+                    possMovesList.add(nextPosition(j, listOfDirections.get(i), position));
+                } else if (board.contains(nextPosition(j, listOfDirections.get(i), position))
+                        && !board.isFree(nextPosition(j, listOfDirections.get(i), position))
+                        && board.getPiece(nextPosition(j, listOfDirections.get(i), position)).getColor() != getColor()) {
+                    possMovesList.add(nextPosition(j, listOfDirections.get(i), position));
+                    break;
+                } else if (board.contains(nextPosition(j, listOfDirections.get(i), position))
+                        && !board.isFree(nextPosition(j, listOfDirections.get(i), position))
+                        && board.getPiece(nextPosition(j, listOfDirections.get(i), position)).getColor() == getColor()) {
+                    break;
+                }
+
+            }
+        }
+        return possMovesList;
     }
 
 }
